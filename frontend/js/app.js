c5b4c9449de9657b5810a792771124c9fe5c5459
Pery-Ram <shujianyaozhang@outlook.com>
2024-03-01 19:55:19 +0100

    guardado 1

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,20 +4,34 @@ const addBtn = document.querySelector("button.btn.btn-light");
 addBtn.addEventListener("click", insertSQL);
 
 async function insertSQL() {
-    const nombre = document.querySelector("div > input.form-control:nth-of-type(1)");
-    const fechaIn = document.querySelector("div > input.form-control:nth-of-type(2)");
-    const fechaFin = document.querySelector("div > input.form-control:nth-of-type(3)");
-    const desc = document.querySelector("div > input.form-control:nth-of-type(4)");
-
+    const nombre = document.querySelector("form input.form-control:nth-of-type(1)");
+    const fechaIn = document.querySelector("form input.form-control:nth-of-type(2)");
+    const fechaFin = document.querySelector("form input.form-control:nth-of-type(3)");
+    const desc = document.querySelector("form input.form-control:nth-of-type(4)");
+    console.log(nombre, fechaIn, fechaFin, desc)
     // comprobador
     const inputs = document.querySelectorAll("inputs.form-control")
     inputs.forEach(input => {
         if (input.value == "") {
-            input.classList.add("input-mal")
+            return
         }
     });
 
-}
+    const url = "http://localhost:3000/insert";
+    await fetch(url, {
+        method: "post",
+        headers: { "Content-Type": "application/json",},
+        body: JSON.stringify({
+            "nombre": nombre.value,
+            "descripcion": desc.value,
+            "fecha_inicio": fechaIn.value,
+            "fecha_fin": fechaFin.value,
+        })
+    })
+        .then(res => res.json())
+        .then(msg => console.log(msg))
+
+}   
 
 
 
