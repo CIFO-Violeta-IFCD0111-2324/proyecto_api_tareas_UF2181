7dab080fb14481bbab6800e77db11b6b17dcfcaf
Pery-Ram <perynew@gmail.com>
2024-03-01 16:34:54 +0100

    v1_03_2024

--- BACKEND/routes.js
+++ BACKEND/routes.js
@@ -2,4 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-const 
+const conexionMySQL = require('./conexionMySQL.js');
+
+router.get("/saludo", (req, res) => {
+  res.json({
+    "saludo": "Hola!"
+  });
+});
+
+router.post("/tarea", (req, res) => {
+ 
+});
+
+////*******Esta línea siempre al final para recoger todo lo ejecutado antes en el mismo archivo ********** */
+
+module.exports = router;
