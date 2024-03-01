1206c67af6a81976240917439a70feb184630522
armandolap <armando.lap@gmail.com>
2024-03-01 15:45:44 +0100

    fix

--- apirest.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS `apirest`;
-CREATE DATABASE `apirest`;
-USE `apirest`;
-CREATE TABLE tasks(id INT PRIMARY KEY AUTO_INCREMENT,
-                   activity VARCHAR(40),
-                   start DATE,
-                   deadline DATE,
-                   comentary VARCHAR(50));
