7f6c6b7b4a3263bcd1bb084370a848b03be56c1a
Pery-Ram <perynew@gmail.com>
2024-03-01 16:30:55 +0100

    v1_03_2024

---
+++ BACKEND/api.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const api = express();
+
+api.use(cors());
+api.use(express.json());
+
+//***************** importar rutas ************************ */
+api.use('./api/v1', require('./routes.js'));
+
+
+//ARRANCAR EL SERVIDOR
+api.listen(port, () => {
+    console.log("Servidor conectado en puerto 3000");
+});
