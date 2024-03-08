a8367ff9963c58cf567b92d6794282cbd5b6b64a
abcnemo <abcnemo1@gmail.com>
2024-03-08 19:33:10 +0100

    08032024_1933

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -72,28 +72,28 @@ fetch("http://localhost:3001/api/v1/leer")
         <div id="resultado">
         <button class="borrar" id="${arrayDatosConsulta[i].id}">borrar</button>
         <button class="editar" id="${arrayDatosConsulta[i].id}">editar</button>
-        <div id="cuadro1">
-        <h2 id="encabezados">ID</h2> 
+        <div class="cuadro1">
+        <h2 class="encabezados">ID</h2> 
         <h3 id="contenido">${arrayDatosConsulta[i].id}</h3>
         </div>
-        <div id="cuadro2">
-        <h2 id="encabezados" >Titulo</h2>
+        <div class="cuadro2">
+        <h2 class="encabezados" >Titulo</h2>
         <h3 id="contenido" class="titulo${arrayDatosConsulta[i].id}">${arrayDatosConsulta[i].titulo}</h3>
         </div>
-        <div id="cuadro2">
-        <h2 id="encabezados">Descripcion</h2>
+        <div class="cuadro2">
+        <h2 class="encabezados">Descripcion</h2>
         <h3 id="contenido">${arrayDatosConsulta[i].descripcion}  </h3>
         </div>
-        <div id="cuadro2">
-        <h2 id="encabezados">portada</h2>
+        <div class="cuadro2">
+        <h2 class="encabezados">portada</h2>
         <h3 id="contenido">${arrayDatosConsulta[i].portada} </h3> 
         </div>
-        <div id="cuadro3">
-        <h2 id="encabezados" >FechaInicio</h2>
+        <div class="cuadro3">
+        <h2 class="encabezados" >FechaInicio</h2>
         <h3 id="contenido">${arrayDatosConsulta[i].fechainicio.substring(0,10)} </h3>
         </div>
-        <div id="cuadro3">
-        <h2 id="encabezados">FechaFinal</h2>
+        <div class="cuadro3">
+        <h2 class="encabezados">FechaFinal</h2>
         <h3 id="contenido">${arrayDatosConsulta[i].fechafinal.substring(0,10)} </h3>
         </div>
         
